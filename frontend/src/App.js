447f40ade3c56582bc67208761d45ffe285ba4b8
abrarZhr <Abrar.alzhrani.ruh.js@tuwaiq.edu.sa>
2021-11-15 15:04:42 +0300

    update log in and reqister componeet

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
-
+import Login from './componet/login/login';
 
 function App() {
   return (
     <div className="App">
+
+    <Login></Login>
       
     </div>
   );
